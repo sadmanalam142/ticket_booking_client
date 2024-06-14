776490ad30998252f25a7c7eeebda89b8b60d0d4
sadmanalam142 <sadmanalam142@gmail.com>
2024-06-14 16:03:20 +0600

    payment responsive done

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -10,10 +10,10 @@ const Payment = () => {
     const booking = useLoaderData();
     const { name, price } = booking;
     return (
-        <div>
-            <h3 className="text-3xl">Payment for {name}</h3>
-            <p className="text-xl">Please pay <strong>${price}</strong></p>
-            <div className='w-96 my-12'>
+        <div className='w-96 mx-auto mt-48 payment_section'>
+            <h3 className="text-3xl">Payment for <span className='text-lime-500'>{name}</span></h3>
+            <p className="text-xl mt-5">Please pay <strong>${price}</strong></p>
+            <div className='w-96 my-12 payment_form'>
                 <Elements stripe={stripePromise}>
                     <CheckoutForm
                         booking={booking}
